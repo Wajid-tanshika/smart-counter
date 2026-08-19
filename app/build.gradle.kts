0724f65d5f8a0ef01655fad3866ea7b8f648732f
Wajid-tanshika <gwajji2212@gmail.com>
2026-08-19 09:58:00 +0530

    Bump versionCode from 2 to 5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
     applicationId = "com.smartcounter.ai"
     minSdk = 24
     targetSdk = 36
-    versionCode = 2
+    versionCode = 5
     versionName = "1.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"

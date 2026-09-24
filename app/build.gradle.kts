6b20f57aefe2a9bfc9e2d523f3f2f409d2017068
Wajid-tanshika <gwajji2212@gmail.com>
2026-09-24 22:27:21 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
         applicationId = "com.smartcounter.ai"
         minSdk = 24
         targetSdk = 36
-        versionCode = 11
+        versionCode = 12
         versionName = "2.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
